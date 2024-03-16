65858f9893724ea3a1ec2528c732c0ce25c90bc7
ProfOak <585087+ProfOak@users.noreply.github.com>
2024-03-16 12:02:13 -0500

    add wezterm config
    In case I ever decide to use wezterm on some other machine I'll have a
    config handy.

---
+++ wezterm.lua
@@ -0,0 +1,44 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.hide_tab_bar_if_only_one_tab = true
+
+config.font = wezterm.font 'Hack'
+config.font_size = 18.0
+config.colors = {
+    cursor_fg = 'black',
+    cursor_bg = 'white',
+    selection_fg = 'black',
+    selection_bg = 'white',
+
+    foreground = '#ffffff',
+    background = '#000000',
+    ansi = {
+        '#000000',
+        '#cc6666',
+        '#66cc99',
+        '#cc9966',
+        '#6699cc',
+        '#cc6699',
+        '#66cccc',
+        '#cccccc',
+    },
+    brights = {
+        '#999999',
+        '#ff9999',
+        '#99ffcc',
+        '#ffcc99',
+        '#99ccff',
+        '#ff99cc',
+        '#99ffff',
+        '#ffffff',
+    }
+}
+
+local act = wezterm.action
+config.keys = {
+    { key = 'DownArrow', mods = 'CTRL', action = act.SpawnTab 'CurrentPaneDomain'},
+    { key = 'LeftArrow', mods = 'CTRL', action = act.ActivateTabRelative(-1)},
+    { key = 'RightArrow', mods = 'CTRL', action = act.ActivateTabRelative(1)},
+}
+return config
