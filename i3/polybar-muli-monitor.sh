0cf082ca9299d8ae60c2382a26ceb427efb44dbd
ProfOak <585087+ProfOak@users.noreply.github.com>
2025-03-18 17:13:12 -0500

    fix systray module on polybar

--- i3/polybar-muli-monitor.sh
+++ i3/polybar-muli-monitor.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-# https://github.com/polybar/polybar/issues/763#issuecomment-331604987
-if type "xrandr"; then
-    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        MONITOR=$m polybar --reload main &
-    done
-else
-    polybar --reload main &
-fi
+# Whichever bar gets created first and requests the systray first gets that systray.
+# Force the primary monitor's bar to get created first.
+# https://github.com/polybar/polybar/issues/1070
+PRIMARY=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
+OTHERS=$(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1)
+declare -a displays=("${PRIMARY[@]}" "${OTHERS[@]}")
+
+for m in "${displays[@]}"; do
+    MONITOR=$m polybar --reload main &
+done
