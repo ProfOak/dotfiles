94c5bec4241bdabfab9786eea4b43beb7f7c2bb2
ProfOak <585087+ProfOak@users.noreply.github.com>
2025-11-04 19:34:52 -0600

    fix yazi issues
    I guess the config file format changed and that caused space selection
    to not work. As part of this I got rid of all the defaults in my
    keymap.toml and started using the ya plugin manager to install
    smart-enter and themes.

--- config/yazi/plugins/smart-enter.yazi/init.lua
+++
@@ -1,8 +0,0 @@
---- @sync entry
--- https://yazi-rs.github.io/docs/tips/#smart-enter
-return {
-	entry = function()
-		local h = cx.active.current.hovered
-		ya.manager_emit(h and h.cha.is_dir and "enter" or "open", { hovered = true })
-	end,
-}
