c77e5b5028b636adad118d8e60668269a7257144
ProfOak <585087+ProfOak@users.noreply.github.com>
2025-11-10 20:52:46 -0600

    Clickable mpd controls on i3bar

--- i3/now_playing.sh
+++ i3/now_playing.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
+
+# Clickable i3bar
+case "$BLOCK_BUTTON" in
+2) mpc toggle &>/dev/null ;; # middle click
+1) mpc next &>/dev/null ;;   # left click
+3) mpc prev &>/dev/null ;;   # right click
+esac
+
 text="$(mpc)"
 if [[ "$(echo "$text" | wc -l)" -eq 1 ]]; then
-    echo ' stopped'
-    return
+  echo ' stopped'
+  return
 fi
 
 song="$(echo "$text" | head -1)"
 status="$(echo "$text" | sed -n '2 p' | grep -oE '(playing|paused)')"
 case "$status" in
-'playing')
-    echo " $song"
-    ;;
-'paused')
-
-    echo " $song"
-    ;;
-*)
-    echo '???'
-    ;;
+'playing') echo " $song" ;;
+'paused') echo " $song" ;;
+*) echo '???' ;;
 esac
